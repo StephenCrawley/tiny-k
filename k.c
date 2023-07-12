a0c235153c14d8ea5ddc7b5b7d3a140d59a305c9
scrawl <scrawl@email.com>
2023-07-12 01:13:52 +0100

    parse prefix op

--- k.c
+++ k.c
@@ -17,19 +17,21 @@ typedef struct k{C t;I r,n;C d[];}*K;
 #define DO(n,x) {I _n=(n),i=0;for(;i<_n;++i){x;}}
 
 // object
-enum {KE=-128,KK=0,KI};
+enum {KE=-128,KK=0,KI,KU};
 K ma(I n){R (K)malloc(sizeof(struct k)+8*n);}
 K tn(C t,I n){K x=ma(n);R xt=t,xr=0,xn=n,x;}
 K ki(I i){K x=tn(-KI,1);R *xi=i,x;} K ke(C a){K x=tn(KE,1);R *xc=a,x;}
+K ku(I i){K x=tn( KU,1);R *xi=i,x;}
 void r0(K x){if(xr--)R;free(x);}
 
 // parse
-I qn(C a){R a>='0'&&a<='9';}
+I qn(C a){R a>='0'&&a<='9';} 
+C vt[]=" +!#,"; I qv(C a){I i=-1;W(vt[++i])if(a==vt[i])R i;R 0;}
 K pi(C**p){C*s=*p;I i=0;W(qn(*s))i=(*s++-'0')+10*i;R *p=s,ki(i);}
-K pe(C *s){W(' '==*s)++s;R qn(*s)?pi(&s):ke(*s);}
+K pe(C *s){W(' '==*s)++s;I i;R qn(*s)?pi(&s):(i=qv(*s))?ku(i):ke(*s);}
 
 // print
-K pr(K x){if(-KI==xt)O("%ld\n",*xi);else O("'%c\n",*xc);R r0(x),x;}
+K pr(K x){-KI==xt?O("%ld",*xi):KU==xt?O("%c:",vt[*xi]):O("'%c",*xc);R O("\n"),r0(x),x;}
 
 // repl
 #define SM 64 //source max
