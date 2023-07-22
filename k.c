3d4129449600ab758ff2368b2260b36a9f6e95fd
scrawl <scrawl@email.com>
2023-07-22 16:40:35 +0100

    now parses infix ops

--- k.c
+++ k.c
@@ -11,6 +11,8 @@ typedef struct k{C t;I r,n;C d[];}*K;
 #define xn    (kN(x))
 #define xc    ((C*)kD(x))
 #define xi    ((I*)kD(x))
+#define xk    ((K*)kD(x))
+#define yt    (kT(y))
 #define W while
 #define O printf
 #define R return
@@ -19,21 +21,29 @@ typedef struct k{C t;I r,n;C d[];}*K;
 C*sc(C *s,C c){W(*s!=c)if(!*s++)R 0;R s;}
 
 // object
-enum {KE=-128,KK=0,KI,KU};
+enum {KE=-128,KK=0,KI,KU,KV};
 K ma(I n){R (K)malloc(sizeof(struct k)+8*n);}
 K tn(C t,I n){K x=ma(n);R xt=t,xr=0,xn=n,x;}
+K k2(K a,K b){K x=tn(KK,2);R *xk=a,xk[1]=b,x;}
+K k3(K a,K b,K c){K x=tn(KK,3);R *xk=a,xk[1]=b,xk[2]=c,x;}
 K ki(I i){K x=tn(-KI,1);R *xi=i,x;} K ke(C a){K x=tn(KE,1);R *xc=a,x;}
-K ku(I i){K x=tn( KU,1);R *xi=i,x;}
-V r0(K x){if(xr--)R;free(x);}
+K ku(I i){K x=tn( KU,1);R *xi=i,x;} K kv(I i){K x=tn(KV,1);R *xi=i,x;}
+V r0(K x){if(xr--)R;if(!xt)DO(xn,r0(xk[i]));free(x);}
+K r1(K x){R xr++,x;}
 
 // parse
-I qn(C a){R a>='0'&&a<='9';} 
+C*ws(C*s){W(' '==*s)++s;R s;} I qn(C a){R a>='0'&&a<='9';} 
 C vt[]=" :+!#,"; I qv(C a){C *s=sc(vt,a);R s?s-vt:0;}
-K pi(C**p){C*s=*p;I i=0;W(qn(*s))i=(*s++-'0')+10*i;R *p=s,ki(i);}
-K pe(C *s){W(' '==*s)++s;I i;R qn(*s)?pi(&s):(i=qv(*s))?ku(i):ke(*s);}
+K pi(C**p){C*s=*p;I i=0;W(qn(*s))i=i*10+*s++-'0';R *p=s,ki(i);}
+K pe(C *s){s=ws(s);K x,y;C a=*s;I v=qv(a);if(!v&&!qn(a))R ke(a);
+ if(v){s=ws(++s);if(!*s)R ke(a);R y=pe(s),KE==yt?y:k2(ku(v),y);}
+ x=pi(&s),s=ws(s),a=*s++;if(!a)R x;v=qv(a);if(!v||!*(s=ws(s)))R r0(x),ke(a);
+ R y=pe(s),KE==yt?r0(x),y:k3(kv(v),x,y);}
 
 // print
-K pr(K x){-KI==xt?O("%ld",*xi):KU==xt?O("%c:",vt[*xi]):O("'%c",*xc);R O("\n"),r0(x),x;}
+V p0(K x){-KI==xt?O("%ld",*xi):KE==xt?O("'%c",*xc):O(KU==xt?"%c:":"%c",vt[*xi]);}
+V pk(K x){if(xt){p0(x);R;}O("(");DO(xn,pk(xk[i]);if(i!=xn-1)O(";"))O(")");}
+K pr(K x){pk(x);O("\n");R r0(x),x;}
 
 // repl
 #define SM 64 //source max
